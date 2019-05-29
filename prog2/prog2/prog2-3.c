8d1cba1b7821d0369dd3d4bcf290bbd1638499f5
naoki709mm <naoki709mm@gmail.com>
2019-05-29 16:02:09 +0900

    a

--- prog2/prog2/prog2-3.c
+++ prog2/prog2/prog2-3.c
@@ -1,60 +1,51 @@
 /*** 奥屋 直己                                                                  
  *** 課題2 問題3:ビンゴプログラム
- *** 提出日 5月22日                                                              
+ *** 提出日 5月22日
+ *** 際提出日 5月27日
+ *** 再々提出日 5月28日
  ***/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-#define N 100
-#define M 100
-#define BNG 5
+#define N 5
+#define M 5
+#define CON 15
 
 int main(void){
-  
+
   int i,j;
-  int num=1;
   int tmp;
-  int max=BNG*3; //数の範囲の最大値
-  int cnt=(int)BNG/2.0; //ビンゴカードの真ん中のマスの値
-  int bng[N][M];
-  int rng[N][M]; //列ごとの数の範囲を格納するはいれつ
+  int cnt=N/2; //ビンゴの列数を2で割ることで、ビンゴ配列の真ん中の要素数となる
+  int bng[N][N];
   int exc[M]; //除外する要素数を格納する配列
   time_t tp;
   
   time(&tp);
   srand48(tp);
   
-  //配列rngを初期化
-  for(i=0 ;i<BNG ;i++){
-    for(j=0 ;j<max ;j++){
-      rng[i][j]=num;
-      num++;
-    }
-  }
-  
   //配列excを0で初期化
-  for(i=0 ;i<BNG ;i++){
-    for(j=0 ;j<max ;j++) 
+  for(i=0 ;i<N ;i++){
+    for(j=0 ;j<N ;j++)
       exc[j]=0;
-    for(j=0 ;j<BNG ;j++){
+    for(j=0 ;j<N ;j++){
       if(i == cnt && j == cnt){ //真ん中の場合，0を入力
-	bng[i][j]=0;
+        bng[i][j]=0;
       }
       else{
-	do{ //選ばれた配列rngの要素が1度現れていないかの判断
-	  tmp=(int)(drand48()*15);
-	}while(exc[tmp] != 0); //1度使われた要素には1を入力
-	exc[tmp]=1;
-	bng[i][j]=rng[i][tmp];
+	do{ //選ばれた要素が1度現れていないかの判断
+          tmp=(int)(drand48()*CON);
+        }while(exc[tmp] != 0); //1度使われた要素には1を入力
+        exc[tmp]=1;
+	bng[i][j]=(tmp+1)+(i*CON); //選ばれた要素は0~14の範囲なので1を足し、i*EXCをたすことで、その列の範囲の値となる
       }
     }
   }
-  
+   
   //ビンゴ風に表示する
-  for(i=0 ;i<BNG ;i++){
-    for(j=0 ;j<BNG ;j++){
+  for(i=0 ;i<N ;i++){
+    for(j=0 ;j<N ;j++){
       if(bng[j][i] == 0)
 	printf("|free");
       else
@@ -62,6 +53,6 @@ int main(void){
     }
     printf("|\n");
   }
-    
+     
   return 0;
-}
+} 
--- prog2/prog2/prog2-4.c
+++ prog2/prog2/prog2-4.c
@@ -1,9 +1,10 @@
 /*** 奥屋 直己                                                                  
- *** 課題2 問題1:内積を計算するプログラム
- *** 提出日 5月17日                                                              
+ *** 課題2 問題4:入力した数字を数値に変換するプログラム
+ *** 提出日 5月27日                                                              
  ***/
 
 #include <stdio.h>
+#include <string.h>
 
 #define N 100
 
@@ -11,6 +12,7 @@ int main(void){
 
   int i=0;
   int dml=0; //小数点の有無
+  int str;
   int bdp=0; //小数点以下の桁数
   double otb=0.0; //小数点以下用の出力
   double otp=0.0; //出力
@@ -19,27 +21,21 @@ int main(void){
   printf("文字列を入力してください:");
   scanf("%s",inp);
   
-  while(inp[i] != '\0'){ //ナル文字までループ
+  str=stren(inp);
+  
+  while(inp[i]){ //ナル文字までループ
     if(inp[i] == '.')
-      dml=1;             //小数点以下が入力された場合,dmlに1を代入
-    switch(dml){         //小数点以下と以上の判定
-    case 0:
+      dml=1;
+    if(dml == 0){
       if(inp[i] >= '0' && inp[i] <= '9')
-	otp=((int)inp[i]-'0')+otp*10.0;
-      break;
-    case 1:              //小数点以下のみの計算
-      if(inp[i] >= '0' && inp[i] <= '9'){
-	otb=((int)inp[i]-'0')+otb*10.0;
-	bdp++;
-      }
-      break;
+	otp=((int)inp[i]-'0')+otp*10;
+    }
+    else{
+      if(inp[i] >= '0' && inp[i] <= '9')
+	
     }
     i++;
   }
-  
-  for(i=0 ;i<bdp ;i++) //小数点以下の値otbを桁数分割る
-    otb/=10.0;
-  otp+=otb;            //小数点以下と整数を足す
   
   if(inp[0] == '-')    //マイナスがあった時、出力を負の値にする
     otp=-otp;
