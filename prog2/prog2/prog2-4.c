61bebe578912592396ee03ebe89b43e681e6db6d
奥屋直己 <lesser@okiyanaokinoMacBook-puro.local>
2019-06-05 14:34:31 +0900

    a

--- prog2/prog2/prog2-4.c
+++ prog2/prog2/prog2-4.c
@@ -1,47 +1,41 @@
-/*** 奥屋 直己                                                                  
+/*** 奥屋 直己                                                      
  *** 課題2 問題4:入力した数字を数値に変換するプログラム
- *** 提出日 5月27日                                                              
+ *** 提出日 5月27日     
+ *** 再提出日 5月29日
+ *** 再々提出日 6月5日
  ***/
-
+ 
 #include <stdio.h>
-#include <string.h>
-
+ 
 #define N 100
-
+ 
 int main(void){
-
+   
   int i=0;
-  int dml=0; //小数点の有無
-  int str;
-  int bdp=0; //小数点以下の桁数
-  double otb=0.0; //小数点以下用の出力
-  double otp=0.0; //出力
+  int pnt=0; //コンマ
+  double dml=1.0;
+  double out=0.0; //出力
   char inp[N]; //入力
-
+  
   printf("文字列を入力してください:");
   scanf("%s",inp);
   
-  str=stren(inp);
-  
-  while(inp[i]){ //ナル文字までループ
+  while(inp[i]){              //ナル文字までループ
     if(inp[i] == '.')
-      dml=1;
-    if(dml == 0){
-      if(inp[i] >= '0' && inp[i] <= '9')
-	otp=((int)inp[i]-'0')+otp*10;
-    }
-    else{
-      if(inp[i] >= '0' && inp[i] <= '9')
-	
+      pnt=1;
+    else if(pnt == 0 && inp[i] >= '0' && inp[i] <= '9') //小数点以上の計算
+      out=((int)inp[i]-'0')+out*10; 
+    else if(pnt == 1 && inp[i] >= '0' && inp[i] <= '9'){
+      dml/=10.0;
+      out+=((int)inp[i]-'0')*dml; 
     }
     i++;
   }
   
   if(inp[0] == '-')    //マイナスがあった時、出力を負の値にする
-    otp=-otp;
-
-  printf("%lf\n",otp);
+    out=-out;
   
-
+  printf("%lf\n",out);
+  
   return 0;
 }
